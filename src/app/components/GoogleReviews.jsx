5180768b2ac778c95015ad27ac9fe5f686e945fd
zuritajonny <jonnyzuritadev@gmail.com>
2023-08-14 11:50:10 -0400

    update("Google - IG section gap")

--- src/app/components/GoogleReviews.jsx
+++ src/app/components/GoogleReviews.jsx
@@ -4,7 +4,11 @@ import React from "react";
 import { ElfsightWidget } from "react-elfsight-widget";
 
 function GoogleReviews() {
-  return <ElfsightWidget widgetID="10cb4c9f-8ef5-4be8-8cc6-277d6caa7c6a" />;
+  return (
+    <div className="google--reviews-wrapper">
+      <ElfsightWidget widgetID="10cb4c9f-8ef5-4be8-8cc6-277d6caa7c6a" />
+    </div>
+  );
 }
 
 export default GoogleReviews;
