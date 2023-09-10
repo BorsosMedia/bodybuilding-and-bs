4b852d7a091c39d55ca46be06b438a763893a969
zuritajonny <jonnyzuritadev@gmail.com>
2023-09-10 12:56:36 -0400

    update('15 call section')

--- src/app/components/ConsultationBox.jsx
+++ src/app/components/ConsultationBox.jsx
@@ -1,25 +1,28 @@
 "use client";
 
-import TopLayer from "../media/top-layer.png";
-import BottomLayer from "../media/bottom-layer.png";
 import { FiArrowUpRight } from "react-icons/fi";
 
 function ConsultationBox() {
   return (
     <div className="inquiry--wrapper consultation--wrapper">
-      <div className="inquiry--block  mn">
-        <h5>GET A FREE 15 MINUTE CONSULTATION CALL</h5>
-        <p>
-          <strong>Let’s chat</strong> about how you are going to reach your
-          goals. No bullshit.
-        </p>
-        <a
-          href="https://payments.bodybuildingandbs.com/phone-consult-15-min"
-          target="_blank"
-          className="button button--secondary"
-        >
-          GO TO THE CALENDAR <FiArrowUpRight className="icon" />
-        </a>
+      <div className="inquiry--inner_wrapper">
+        <div className="consulation-img_wrapper ">
+          {/*  <Image src={Nick} alt="" className="" /> */}
+        </div>
+        <div className="inquiry--block  mn">
+          <h5>GET A FREE 15 MINUTE CONSULTATION CALL</h5>
+          <p>
+            <strong>Let’s chat</strong> about how you are going to reach your
+            goals. No bullshit.
+          </p>
+          <a
+            href="https://payments.bodybuildingandbs.com/phone-consult-15-min"
+            target="_blank"
+            className="button button--secondary"
+          >
+            GO TO THE CALENDAR <FiArrowUpRight className="icon" />
+          </a>
+        </div>
       </div>
     </div>
   );
